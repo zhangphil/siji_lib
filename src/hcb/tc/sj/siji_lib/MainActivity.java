7bc9d9333d81f7bea4f7083f1a73fa66ac2ab2ed
zhangphil <zhang.fei@msn.com>
2015-10-14 15:18:37 +0800

    first

---
+++ src/hcb/tc/sj/siji_lib/MainActivity.java
@@ -0,0 +1,12 @@
+package hcb.tc.sj.siji_lib;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
